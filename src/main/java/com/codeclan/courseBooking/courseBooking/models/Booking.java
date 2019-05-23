f176c5117b9f7a52fcb2b7221294cb1cc3ac1c6d
Mark <nirvanafoo86@hotmail.com>
2019-05-23 11:02:38 +0100

    Created Booking class with attributes and getters/setters

--- src/main/java/com/codeclan/courseBooking/courseBooking/models/Booking.java
+++ src/main/java/com/codeclan/courseBooking/courseBooking/models/Booking.java
@@ -1,4 +1,57 @@
 package com.codeclan.courseBooking.courseBooking.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "booking")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String date;
+
+    @JsonIgnoreProperties("booking")
+    @OneToMany(mappedBy = "booking", fetch = FetchType.LAZY)
+    Course course;
+
+    @JsonIgnoreProperties("booking")
+    @OneToMany(mappedBy = "booking", fetch = FetchType.LAZY)
+    @Column
+    Customer customer;
+
+    public Booking(String date, Course course, Customer customer) {
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
